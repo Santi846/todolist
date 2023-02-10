483e1cbdb6fccecd1057cbfc4ebd85d4c047fc7f
Santiago Arocha <arochas846@gmail.com>
2023-02-10 20:36:48 -0300

    Complete function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import { useState } from 'react';
 import './App.css';
 import { Task } from './components/task.jsx';
 
 function App() {
+
+  const [isComplete, setCompletement] = useState(false);
+
+  const setState = () => {
+    setCompletement(true);
+  }
   return (
     <div className="App">
       <h1 className='title'>Tareas para hacer</h1>
       <div className='todo_list'>
       <h1 className='mini_title'>Mis tareas</h1>
-      <Task text='Aprender React'></Task>
+      <Task text='Aprender React'complete={setState}></Task>
       </div>
     </div>
   );
--- src/components/task.jsx
+++ src/components/task.jsx
@@ -4,14 +4,27 @@ import { AiOutlineBorder, AiOutlineCheckSquare } from "react-icons/ai";
 
 
 
-export function Task ({text}) {
+export function Task ({text, complete, setState}) {
+
+    if (setState) {
+        return (
+            <div className= 'task_container complete'>
+                <div className="task_text">
+                    {text}
+                </div>
+                <div className="task_icons_container">
+                <AiOutlineCheckSquare className="task_icon_check_on"></AiOutlineCheckSquare>           
+                </div>
+            </div>
+        );
+    }
     return (
-        <div className="task_container">
+        <div className= 'task_container'>
             <div className="task_text">
                 {text}
             </div>
-            <div className="task_icon">
-            <AiOutlineBorder></AiOutlineBorder>           
+            <div className="task_icons_container">
+            <AiOutlineBorder className="task_icon_check_off"></AiOutlineBorder>           
             </div>
         </div>
     );
